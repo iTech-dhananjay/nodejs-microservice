3e301b668fa19af78399f1b0777dd83408e1de82
Dhananjay <officialdhananjay76@gmail.com>
2024-07-23 17:29:44 +0530

    Add video-stream api

--- src/modules/video-stream/router/upload.js
+++ src/modules/video-stream/router/upload.js
@@ -1,29 +1,9 @@
+// routes/videoRouter.js
 import { Router } from 'express';
-import multer from 'multer';
-import fs from 'fs';
-import path from 'path';
 import { uploadVideo } from '../services/stream.js';
+import upload from '../../../config/multerFile.js'; // Adjust the path as necessary
 
 const router = Router();
-
-const uploadDir = 'public/uploads';
-
-// Ensure the upload directory exists
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-// Configure multer to save files in the 'public/uploads' directory
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
-});
-
-const upload = multer({ storage });
 
 router.post('/', upload.single('video'), async (req, res) => {
     if (!req.file) {
