4f465bbec07e13b96c49c24fb5d3f15031c2356d
Dhananjay <officialdhananjay76@gmail.com>
2024-07-26 14:45:15 +0530

    Add default permission for user

--- src/modules/ecom-aws-cloud/router/user.js
+++ src/modules/ecom-aws-cloud/router/user.js
@@ -65,10 +65,6 @@ router.post('/login', async (req, res) => {
           };
 
           return res.status(200).json(response);
-
-          return res.status(200).json(response);
-
-          return res.status(200).json(response);
      } catch (error) {
           console.error(error);
           return res.status(500).json({ message: 'Internal server error' });
