5e9872866d9f42d40e9a6fefec6ec18fe5cf4edf
Dhananjay <officialdhananjay76@gmail.com>
2024-07-30 17:41:31 +0530

    Generate qr code

--- src/utils/qrCodeGenerator.js
+++ src/utils/qrCodeGenerator.js
@@ -1,10 +1,17 @@
 import QRCode from 'qrcode';
 import path from 'path';
 
+
+// Function to generate a QR code
 const generateQRCode = async (text, filename) => {
     try {
-        const outputFilePath = path.resolve('public/qrcodes', filename);
+        // Construct the full path for the output file
+        const outputFilePath = path.resolve('public/qrcodes', filename); // [ /Users/dhananjaykumar/Desktop/Workspace/Projects/node-microservice/server/public/qrcodes/66a3bad4f9ed83696c40fa62.png ]
+
+        // Generate the QR code and save it to the specified file path
         await QRCode.toFile(outputFilePath, text);
+
+        //Return the file path of the generated QR code
         return outputFilePath;
     } catch (err) {
         console.error('Error generating QR code:', err);
